1df0634083d21f1f7c3c2b333a6c4a9203421f18
arjunbector <arjunbector@gmail.com>
2024-03-09 16:51:12 +0530

    add challenges part to the form

--- frontend/components/LandingPage/FormSection/FormSection.jsx
+++ frontend/components/LandingPage/FormSection/FormSection.jsx
@@ -4,6 +4,7 @@ import rightVector from "../../../public/landingPage/form/rightVector.svg";
 import bottomVector from "../../../public/landingPage/form/bottomVector.svg";
 import Form from "./Form";
 import healthCompArr from "@/constants/complications";
+import challenges from "@/constants/challenges";
 import { useState } from "react";
 import Capsule from "./Capsule";
 
@@ -23,12 +24,31 @@ const FormSection = () => {
   });
 
   const [healthDataToSend, setHealthDataToSend] = useState([]);
-  console.log(healthDataToSend);
   const healthData = [...healthCompArr];
-  const capsules = healthData.map((complication) => (
-    <Capsule text={complication.name} selected={complication.selected} healthDataToSend={healthDataToSend} setHealthDataToSend={setHealthDataToSend} healthData={healthData} />
+  const healthCapsules = healthData.map((complication) => (
+    <Capsule
+      text={complication.name}
+      selected={complication.selected}
+      healthDataToSend={healthDataToSend}
+      setHealthDataToSend={setHealthDataToSend}
+      healthData={healthData}
+    />
   ));
 
+  const [challengesDataToSend, setChallengesDataToSend] = useState([]);
+  const challengesData = [...challenges];
+  const challengesCapsules = challengesData.map((complication) => (
+    <Capsule
+      text={complication.name}
+      selected={complication.selected}
+      healthDataToSend={challengesDataToSend}
+      setHealthDataToSend={setChallengesDataToSend}
+      healthData={challengesData}
+    />
+  ));
+
+  console.log(healthDataToSend);
+  console.log(challengesDataToSend);
   console.log(formData);
   return (
     <section className="min-h-screen bg-[#F5EEE6] relative overflow-x-hidden flex justify-center items-center">
@@ -45,9 +65,11 @@ const FormSection = () => {
         <Form formData={formData} setFormData={setFormData} />
         <div className="my-16 mx-28 p-10 bg-[#FFF8E3] flex flex-col justify-start rounded-lg">
           <h1 className="m-1 font-semibold mb-3">Health Complications</h1>
-          <div className="flex flex-wrap gap-4">
-         {capsules}
-          </div>
+          <div className="flex flex-wrap gap-4">{healthCapsules}</div>
+        </div>
+        <div className="my-16 mx-28 p-10 bg-[#FFF8E3] flex flex-col justify-start rounded-lg">
+          <h1 className="m-1 font-semibold mb-3">Expected Challenges you foresee when returning to work </h1>
+          <div className="flex flex-wrap gap-4">{challengesCapsules}</div>
         </div>
       </div>
     </section>
