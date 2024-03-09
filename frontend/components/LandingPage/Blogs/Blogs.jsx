8ceff71f7ca2068e72a89950c854eb1f31092d9f
arjunbector <arjunbector@gmail.com>
2024-03-09 22:34:46 +0530

    style: navbar changes

---
+++ frontend/components/LandingPage/Blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <section className='min-h-screen'>
+      <h1>Blogs</h1>
+    </section>
+  )
+}
+
+export default Blogs
--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Navbar = () => {
   const { status } = useSession();
   return (
-    <nav>
+    <nav className="flex w-full sticky backdrop-blur-sm h-16 z-[10] top-0 justify-end p-5 gap-10 items-center bg-[#f3d7ca8e]">
+      <Link href="/">Home</Link>
+      <Link href="/">Blogs</Link>
+      <Link href="/">About Us</Link>
+      <Link href="/">Dashboard</Link>
       {status === "authenticated" ? (
         <button
+          classname="bg-[#E6A4B4]"
           onClick={() => {
             signOut();
           }}
@@ -13,7 +19,7 @@ const Navbar = () => {
           Logout
         </button>
       ) : (
-        <button
+        <button className="bg-[#E6A4B4] px-2 py-1 rounded-md"
           onClick={() => {
             signIn();
           }}
