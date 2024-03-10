170c8866e4a400f617d9d41e0c9c3ad945f68351
arjunbector <arjunbector@gmail.com>
2024-03-10 10:09:45 +0530

    integrate form

--- frontend/pages/api/gemini/getData.js
+++ frontend/pages/api/gemini/getData.js
@@ -4,20 +4,23 @@ import { UsersDetails } from "@/models/Userdetails";
 
 import getTokenDetails from "@/utils/auth";
 import { connectMongoDB } from "@/lib/mongodb";
-import {Users} from "../../../models/user"
-
+import { Users } from "../../../models/user";
 
 export default async function handler(req, res) {
-   await  connectMongoDB();
+  try {
+    await connectMongoDB();
     const auth = req.headers.authorization.split(" ")[1];
     let userId = await getTokenDetails(auth);
-    console.log("fffgf",userId);
-    const user=await Users.findById(userId);
-    console.log("USER===",user);
+    console.log("fffgf", userId);
+    const user = await Users.findById(userId);
+    console.log("USER===", user);
 
-   
-    const dailyTasks=user.daily;
-    const weeklyTasks=user.weekly;
-    res.status(200).json({dailyTasks:dailyTasks,weeklyTasks:weeklyTasks});
-
+    const dailyTasks = user.daily;
+    const weeklyTasks = user.weekly;
+    return res
+      .status(200)
+      .json({ dailyTasks: dailyTasks, weeklyTasks: weeklyTasks });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
